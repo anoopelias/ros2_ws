6bebc102e284ff66e98b93b790ca938e59990291
anoopelias <anoopelias@gmail.com>
2024-05-08 09:59:42 +0530

    Add service to reset number counter in py

--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node
 
 from example_interfaces.msg import Int64
-
+from example_interfaces.srv import SetBool
 
 class NumberCounterNode(Node):
     def __init__(self):
@@ -12,6 +12,8 @@ class NumberCounterNode(Node):
         self.subscriber = self.create_subscription(
                 Int64, "number", self.callback_number, 10)
         self.publisher = self.create_publisher(Int64, "number_count", 10)
+        self.server = self.create_service(SetBool, "reset_counter",
+                self.callback_reset_counter)
         self.get_logger().info("Number counter has started")
 
     def callback_number(self, number_msg):
@@ -20,6 +22,16 @@ class NumberCounterNode(Node):
         count_msg.data = self.count
         self.publisher.publish(count_msg)
         self.get_logger().info("Publishing count " + str(self.count))
+
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            self.count = 0
+            response.message = "Reset count to zero"
+        else:
+            response.message = "No change"
+        response.success = True
+        self.get_logger().info(response.message)
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
