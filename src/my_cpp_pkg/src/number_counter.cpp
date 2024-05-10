1dcde2f8a2f250a8a3d8e6a195c439f36b492489
anoopelias <anoopelias@gmail.com>
2024-05-10 09:48:43 +0530

    Add service to reset number counter in cpp

--- src/my_cpp_pkg/src/number_counter.cpp
+++ src/my_cpp_pkg/src/number_counter.cpp
@@ -1,5 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
 
 class NumberCounterNode : public rclcpp::Node
 {
@@ -9,6 +13,9 @@ public:
         subscriber = this->create_subscription<example_interfaces::msg::Int64>("number", 10,
                 std::bind(&NumberCounterNode::callbackNumber, this, std::placeholders::_1));
         publisher = this->create_publisher<example_interfaces::msg::Int64>("number_count", 10);
+        server = this->create_service<example_interfaces::srv::SetBool>(
+                "reset_counter",
+                std::bind(&NumberCounterNode::callbackResetCounter, this, _1, _2));
         RCLCPP_INFO(this->get_logger(), "Number counter has started");
     }
 
@@ -19,13 +26,26 @@ private:
         auto msg = example_interfaces::msg::Int64();
         msg.data = this->count;
         publisher->publish(msg);
-        RCLCPP_INFO(this->get_logger(), "Publishing count");
+        RCLCPP_INFO(this->get_logger(), "Publishing count %ld", msg.data);
+    }
+
+    void callbackResetCounter(const example_interfaces::srv::SetBool::Request::SharedPtr request,
+            const example_interfaces::srv::SetBool::Response::SharedPtr response)
+    {
+        if (request->data) {
+            this->count = 0;
+            response->message = "Reset count to zero";
+        } else {
+            response->message = "No change";
+        }
+        RCLCPP_INFO(this->get_logger(), "Reset counter: %s", response->message.c_str());
     }
 
     int count;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher;
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscriber;
     rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server;
 };
 
 int main(int argc, char **argv)
