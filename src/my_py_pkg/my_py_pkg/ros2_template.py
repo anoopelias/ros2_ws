4da69ca59c0ec31a11ea448d0ec39fdacc686e7f
anoopelias <aelias@niqorobotics.com>
2025-08-03 13:52:03 +0530

    Add ros2 tempalte as a package

--- src/my_py_pkg/my_py_pkg/ros2_template.py
+++ src/my_py_pkg/my_py_pkg/ros2_template.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 
 class MyCustomNode(Node): # MODIFY NAME
     def __init__(self):
-        super().__init__("node_name") # MODIFY NAME
+        super().__init__("ros2_template") # MODIFY NAME
 
 
 def main(args=None):
--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -27,7 +27,8 @@ setup(
             "number_counter = my_py_pkg.number_counter:main",
             "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
             "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main",
-            "add_two_ints_client = my_py_pkg.add_two_ints_client:main"
+            "add_two_ints_client = my_py_pkg.add_two_ints_client:main",
+            "ros2_template = my_py_pkg.ros2_template:main"
         ],
     },
 )
